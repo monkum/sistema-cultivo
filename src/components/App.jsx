fcfceb0cf848506aaeeaec68caf985290fbfe37a
Henry Cerpa <henrycerpa@gmail.com>
2021-11-18 15:49:44 -0500

    Intento de RUN START

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
+import '../styles/global.scss';
 
 const App = () => {
-  return (
-      <h1> Hola Mundo!</h1>
-  
-  );
+	return (
+		<h1>Hola Mundo, Bienvenidos al Curso Practico de React!</h1>
+	);
 }
 
 export default App;
