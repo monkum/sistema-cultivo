51b0f14df0429f8981c190590b7d1abdd4c2d06b
Henry Cerpa <henrycerpa@gmail.com>
2021-11-27 20:50:03 -0500

    Bootstrap Funcional

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/global.scss';
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
 	return (
--- src/containers/login.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Login = () => {
-    return (
-        <h1>Hola Mundo! </h1>
-    );
-}
