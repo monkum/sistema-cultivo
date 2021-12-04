0ffbec77c363b40b21b420dc74a472bf05c098f2
reinosoe <reinosoe@uninorte.edu.co>
2021-12-03 19:18:58 -0500

    listo el home!

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,39 +1,43 @@
 import React from "react";
 import "../styles/Home.css";
+import fondo from "../styles/cultivo.jpg"
 
 
 const Home = () => {
     return (
-        <div id="contenedor">
-            <div className="titulo">
-                <h1>AGRICOLOMBIA S.A.S.</h1>
-            </div>
-            <div className="login">
-                <div id="login-form" className="login-form">
-                    <form action>
-                        <ul>
-                            <li className="filas">
-                                <p>
-                                    <label htmlFor="user" className="user" /> Usuario:
-                                    <input type="text" id="usuario" name="usuario" maxLength={50} />
-                                </p><p>
-                                </p></li>
-                                    <li className="filas">
-                                <p>
-                                    <label htmlFor="contrasena" />Contraseña: 
-                                    <input type="password" name="password" id="password" /> 
-                                </p><p>
-                                </p></li>
-                                    <li className="filas">
-                                <p>
-                                    <input type="submit" defaultValue="Ingresar" className="ingresar" /> 
-                                </p>
-                            </li>
-                        </ul>
-                        <div className="small-6 columns text-right">
-                            <a href>Olvidaste tu contraseña</a>
-                        </div>
-                    </form>
+        <div className="contenedor">
+            <img src={fondo} alt="" />
+            <div id="contenedor">
+                <div className="titulo">
+                    <h1>AGRICOLOMBIA S.A.S.</h1>
+                </div>
+                <div className="login">
+                    <div id="login-form" className="login-form">
+                        <form action>
+                            <ul>
+                                <li className="filas">
+                                    <p>
+                                        <label htmlFor="user" className="user" /> Usuario:
+                                        <input type="text" id="usuario" name="usuario" maxLength={50} />
+                                    </p>
+                                </li>
+                                <li className="filas">
+                                    <p>
+                                        <label htmlFor="contrasena" />Contraseña:
+                                        <input type="password" name="password" id="password" />
+                                    </p>
+                                </li>
+                                <li className="filas">
+                                    <p>
+                                        <input type="submit" defaultValue="Ingresar" className="ingresar" />
+                                    </p>
+                                </li>
+                            </ul>
+                            <div className="small-6 columns text-right">
+                                <a href>Olvidaste tu contraseña</a>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
